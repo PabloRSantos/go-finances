83c39e2750a31090d00467aa64f433c9512df959
Pablo <pablorsantos15@gmail.com>
2021-09-24 16:04:10 -0300

    feat: transactions list

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,21 +1,96 @@
-import React from 'react'
-import * as S from './styles'
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
+import * as S from "./styles";
+
+export interface DataListProps extends TransactionCardProps {
+    id: string
+}
 
 export const Dashboard: React.FC = () => {
-    return (
-        <S.Container>
-            <S.Header>
-                <S.UserWrapper>
-                    <S.UserInfo>
-                        <S.Photo source={{ uri: 'https://avatars.githubusercontent.com/u/64219605?v=4' }}/>
-                        <S.User>
-                            <S.UserGreeting>Olá, </S.UserGreeting>
-                            <S.UserName>Rodrigo</S.UserName>
-                        </S.User>
-                    </S.UserInfo>
-                    <S.Icon name="power" />
-                </S.UserWrapper>
-            </S.Header>
-        </S.Container>
-    )
-}
+  const data: DataListProps[] = [
+    {
+      id: '1',
+      type: "positive",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "dollar-sign",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: '2',
+      type: "negative",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "coffee",
+      },
+      date: "13/04/2020",
+    },
+    {
+      id: '3',
+      type: "negative",
+      title: "Desenvolvimento de site",
+      amount: "R$ 12.000,00",
+      category: {
+        name: "Vendas",
+        icon: "shopping-bag",
+      },
+      date: "13/04/2020",
+    },
+  ];
+
+  return (
+    <S.Container>
+      <S.Header>
+        <S.UserWrapper>
+          <S.UserInfo>
+            <S.Photo
+              source={{
+                uri: "https://avatars.githubusercontent.com/u/64219605?v=4",
+              }}
+            />
+            <S.User>
+              <S.UserGreeting>Olá, </S.UserGreeting>
+              <S.UserName>Rodrigo</S.UserName>
+            </S.User>
+          </S.UserInfo>
+          <S.Icon name="power" />
+        </S.UserWrapper>
+      </S.Header>
+
+      <S.HighlightCards>
+        <HighlightCard
+          title="Entradas"
+          amount="R$ 17.400,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="up"
+        />
+        <HighlightCard
+          title="Saídas"
+          amount="R$ 1.259,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="down"
+        />
+        <HighlightCard
+          title="Total"
+          amount="R$ 16.141,00"
+          lastTransaction="Última entrada dia 13 de abril"
+          type="total"
+        />
+      </S.HighlightCards>
+      <S.Transactions>
+        <S.Title>Listagem</S.Title>
+        <S.TransactionList
+          data={data}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <TransactionCard data={item} />}
+        />
+      </S.Transactions>
+    </S.Container>
+  );
+};
