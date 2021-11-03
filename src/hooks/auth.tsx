2657dff33d757bc4c88d1cda7f2cc70f985502ab
Pablo <pablorsantos15@gmail.com>
2021-11-03 14:19:19 -0300

    feat: google auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,30 +1,58 @@
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
+import * as AuthSession from "expo-auth-session";
+
+interface AuthorizationResponse {
+    params: {
+        access_token: string
+    }
+    type: string
+}
 
 interface User {
-    id: string
-    name: string
-    email: string
-    photo?: string
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
 }
 
 interface IAuthContextData {
-    user: User
+  user: User;
+  signInWithGoogle: () => Promise<void>
 }
 
-const AuthContext = createContext<IAuthContextData>({} as IAuthContextData );
+const AuthContext = createContext<IAuthContextData>({} as IAuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
-  const user = {
-      id: '123',
-      name: 'Pablo',
-      email: 'pablo@mail.com'
-  }
+const [user, setUser] = useState<User>({} as User)
 
-  return <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>;
+  async function signInWithGoogle() {
+    const CLIENT_ID = "787992196104-1vkbm8ul7gcob5trf70ra13c57uirid1.apps.googleusercontent.com";
+    const REDIRECT_URI = "https://auth.expo.io/@pablorsantos/gofinances";
+    const RESPONSE_TYPE = "token";
+    const SCOPE = encodeURI("profile email");
+    const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+    const { params, type } = await AuthSession.startAsync({ authUrl }) as AuthorizationResponse;
+
+    if(type === 'success') {
+        const response = await fetch(`https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`)
+        const userInfo = await response.json()
+        setUser({
+            id: userInfo.id,
+            email: userInfo.email,
+            name: userInfo.giver_name,
+            photo: userInfo.picture
+        })
+    }
+}
+
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle }}>{children}</AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
-    const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
 
-    return context
-}
+  return context;
+};
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,32 +1,49 @@
-import React from 'react';
-import * as S from './styles';
-import AppleSvg from '../../assets/apple.svg'
-import GoogleSvg from '../../assets/google.svg'
-import LogoSvg from '../../assets/logo.svg'
-import { RFValue } from 'react-native-responsive-fontsize';
-import { SignInSocialButton } from '../../components/SignInSocialButton';
-import { useAuth } from '../../hooks/auth';
+import React from "react";
+import * as S from "./styles";
+import AppleSvg from "../../assets/apple.svg";
+import GoogleSvg from "../../assets/google.svg";
+import LogoSvg from "../../assets/logo.svg";
+import { RFValue } from "react-native-responsive-fontsize";
+import { SignInSocialButton } from "../../components/SignInSocialButton";
+import { useAuth } from "../../hooks/auth";
+import { Alert } from "react-native";
 
 export const SignIn: React.FC = () => {
-  const { user } = useAuth()
+  const { signInWithGoogle } = useAuth();
+
+  async function handleSignInWithGoogle() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possivel conectar a conta Google");
+    }
+  }
 
   return (
     <S.Container>
       <S.Header>
         <S.TitleWrapper>
           <LogoSvg width={RFValue(120)} height={RFValue(68)} />
-          <S.Title>Controle suas{'\n'}finanças de forma{'\n'}muito simples</S.Title>
+          <S.Title>
+            Controle suas{"\n"}finanças de forma{"\n"}muito simples
+          </S.Title>
         </S.TitleWrapper>
-        <S.SignInTitle>Faça seu login com{'\n'}uma das contas abaixo</S.SignInTitle>
+        <S.SignInTitle>
+          Faça seu login com{"\n"}uma das contas abaixo
+        </S.SignInTitle>
       </S.Header>
 
-      <S.Footer >
+      <S.Footer>
         <S.FooterWrapper>
-           <SignInSocialButton svg={GoogleSvg} title="Entrar com Google" />
-           <SignInSocialButton svg={AppleSvg} title="Entrar com Apple" />
+          <SignInSocialButton
+            onPress={handleSignInWithGoogle}
+            svg={GoogleSvg}
+            title="Entrar com Google"
+          />
+          <SignInSocialButton svg={AppleSvg} title="Entrar com Apple" />
         </S.FooterWrapper>
       </S.Footer>
     </S.Container>
   );
 };
-
